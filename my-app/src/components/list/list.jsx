af4b6ffdd5a4b2772bd374c903df9982d49ffe9c
c <rladuddlf3@naver.com>
2021-05-09 23:47:58 +0900

    to move

--- my-app/src/components/list/list.jsx
+++ my-app/src/components/list/list.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./list.module.css";
 
 const List = ({title,description,link,maintitle}) => {
+  const fooBarNode = useRef(null);
+
+  const handleList = (event) => {
+    event.preventDefault();
+    console.log(event.target.dataset.name);
+  }
+
 
   return (
-      <li className={styles.title}>{title}</li>
+      <li ref={fooBarNode} onClick={handleList} className={styles.title} data-name={title} >{title}</li>
   )
 }
 
--- my-app/src/components/maindata/maindata.jsx
+++ my-app/src/components/maindata/maindata.jsx
@@ -4,7 +4,7 @@ import styles from "./maindata.module.css";
 const MainData = ({title, description, link}) => {
   return (
     <>
-      <h2 className={styles.title}>{title}</h2>
+      <h2 className={`${styles.title}` }>{title}</h2>
       <hr></hr>
       <h3 className={styles.description}>{description}</h3>
       {link ? <h3 className={styles.link}>출처: {link}</h3> : <br/>}
