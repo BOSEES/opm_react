6a6279f33b7708f1fdfb7da536bdd9d4fb397442
c <rladuddlf3@naver.com>
2021-05-11 19:01:02 +0900

    과연

--- my-app/src/components/house/house.jsx
+++ my-app/src/components/house/house.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./house.module.css";
 import SidebarMenu from "../sidebar_menu/sidebarMenu";
 import Description from "../description/description";
 
 const House = ({data}) => {
+  const [textValue, setTextValue] = useState();
+
   return (
     <>
     <img className={styles.backImage} src="/images/banner.png" alt="사진"></img>
       <section className={styles.house}>
       <SidebarMenu data={data}/>
       <div className={styles.description}>
-      <Description data={data}/>
+      <Description data={data} setTextValue={setTextValue}/>
       </div>
     </section>
     </>
--- my-app/src/components/maindata/maindata.jsx
+++ my-app/src/components/maindata/maindata.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./maindata.module.css";
 
 const MainData = ({title, description, link}) => {
+  const targetNode = useRef();
+
   return (
     <>
-      <h2 className={`${styles.title}` }>{title}</h2>
+      <h2 className={`${styles.title} ${title}`}>{title}</h2>
       <hr></hr>
       <h3 className={styles.description}>{description}</h3>
       {link ? <h3 className={styles.link}>출처: {link}</h3> : <br/>}
